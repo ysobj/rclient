74657602609ad8ec63d23cef4ecd632f18d92db7
ysobj <litailang+ysobj@gmail.com>
2016-04-20 13:16:05 +0900

    20160420

--- src/message.js
+++ src/message.js
@@ -1,21 +1,34 @@
 "use strict";
 var React = require("react");
+var axios = require("axios");
 var Message = React.createClass({
+  getInitialState: function() {
+    return {
+      comments: []
+    };
+  },
   render: function() {
-        var renderMessage = function() {
+        var renderMessage = function(data) {
+          if(data == null){
+            return "";
+          }
                   return (
       <div>
         <div className="message-container">
           <div className="avatar"><img alt="avatar" src='http://www.gravatar.com/avatar/13f5b1351a04db4c83e60fcf7fb6d6ae?rating=PG&size=50&default=wavatar'/></div>
           <div className="comment-info"><div>ysobj</div>3/11 12:40</div></div>
-        <div>いろはにほへと ちりぬぬを</div>
+        <div>{data.contents}</div>
       </div>
                     );
                 }
     return <div>
-      {renderMessage()}
-      {renderMessage()}
+      {this.state.comments.map(renderMessage, this)}
     </div>;
+  },
+  componentDidMount: function() {
+    axios.get("/api/comments/").then(function(res){
+      this.setState({comments: res.data});
+    }.bind(this));
   }
 });
 module.exports = Message;
