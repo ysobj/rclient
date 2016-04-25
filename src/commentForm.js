7df912ef43cb7492161d68475c55340155751cc8
ysobj <litailang+ysobj@gmail.com>
2016-04-25 13:07:58 +0900

    20160425

--- src/commentForm.js
+++ src/commentForm.js
@@ -1,16 +1,25 @@
 "use strict";
 var React = require("react");
+var ReactDom = require("react-dom");
 var axios = require("axios");
 var CommentForm = React.createClass({
   handleClickSend: function() {
-    console.log("onClick!(CommentForm)");
+    var comment = ReactDom.findDOMNode(this.refs.comment).value;
+    console.log("onClick!(CommentForm)", comment);
+    axios.post("/api/comments/",{
+      contents: comment,
+      regDate: new Date(),
+      userId: 1
+    }).then(function(response){
+      console.log(response);
+    });
   },
   render: function(){
     return <form className="form-horizontal" action="../users">
         <div className="form-group">
           <label htmlFor="inputComment" className="col-lg-2 control-label">Comment</label>
           <div className="col-lg-6">
-            <input type="text" className="form-control" id="inputComment" placeholder="Comment"></input>
+            <input ref="comment" type="text" className="form-control" id="inputComment" placeholder="Comment"></input>
           </div>
           <span onClick={this.handleClickSend} className="btn btn-primary">send</span>
         </div>
