a8f5d3ce3d14a7872afd99e25629af951565436a
ysobj <litailang+ysobj@gmail.com>
2016-05-06 13:00:23 +0900

    20160506

--- src/message.js
+++ src/message.js
@@ -7,11 +7,13 @@ var Message = React.createClass({
   render: function(){
     var data = this.props;
     return (
-      <div>
-        <div className="message-container">
+      <div className="message-container">
+        <div className="message-body">
           <div className="avatar"><img alt="avatar" src='http://www.gravatar.com/avatar/13f5b1351a04db4c83e60fcf7fb6d6ae?rating=PG&size=50&default=wavatar'/></div>
-          <div className="comment-info"><span>ysobj</span><span>{data.regDate}</span></div>
-          <div>{data.contents}</div>
+          <div>
+            <div className="comment-info"><span>ysobj</span><span>{data.regDate}</span></div>
+            <div className="comment-body">{data.contents}</div>
+          </div>
         </div>
       </div>);
   }
